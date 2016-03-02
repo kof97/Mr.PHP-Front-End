0cd85f2e0257cd9cebcf1ab0de6653b398443f4c
kof97 <1048434786@qq.com>
2016-03-02 17:28:02 +0800

    update resume

--- application/controller/index.php
+++ application/controller/index.php
@@ -1,16 +1,15 @@
 <?php 
 class Index extends MrController
 {
-	public function show()
-	{
-		$this->view("index.html");
-	}
+    public function show()
+    {
+        $this->view("index.html");
+    }
 
-	public function doc()
-	{
-		$this->view("doc.html");
-	}
-
+    public function doc()
+    {
+        $this->view("doc.html");
+    }
 
 }
 
